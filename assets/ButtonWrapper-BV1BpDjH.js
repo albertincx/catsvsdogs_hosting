b85942aca282c6d95993ec63feb92c5b49de9662
Albert Safiullin <albertincx@gmail.com>
2024-11-23 22:05:15 +0700

    init

--- assets/ButtonWrapper-BV1BpDjH.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as s,T as o}from"./index-GomyT9KN.js";const t=()=>{const t=n();return s.jsx(o,{className:"ton-conn purple "+(t?"":"no-connect")})};export{t as ButtonWrapper,t as default};
